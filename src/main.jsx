0b81cc189429225efdd0d73aac6544e077ed3203
Sarath_Kumar_S <sarathmca2016@gmail.com>
2025-07-08 12:18:57 +0530

    IRAI-0: its for git and godaddy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Dev-Iraitchi">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
